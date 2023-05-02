d3f1c603769b1847a7ea034d596d3f1100a74795
tc-user <tc-user@A-499.local>
2023-05-03 04:07:46 +0900

    実装１

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,15 @@
+class PrototypesController < ApplicationController
+  before_action :set_prototype, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+
+  def index
+ 
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+end
